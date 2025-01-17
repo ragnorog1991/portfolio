c285cc5ba27e5697324dfa0554913b9ae8b78d40
Stanislav <wolf9107@mail.ru>
2025-01-17 10:05:49 +0300

    update animation

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
-const hamburger = document.querySelector('.hamburger'),
-    menu = document.querySelector('.menu'),
-    closeElem = document.querySelector('.menu__close');
+const hamburger = document.querySelector(".hamburger"),
+  menu = document.querySelector(".menu"),
+  closeElem = document.querySelector(".menu__close");
 
-hamburger.addEventListener('click', () => {
-
-    menu.classList.add('active');
+hamburger.addEventListener("click", () => {
+  menu.classList.add("active");
 });
 
-closeElem.addEventListener('click', () => {
-
-    menu.classList.remove('active');
+closeElem.addEventListener("click", () => {
+  menu.classList.remove("active");
 });
 
-const counters = document.querySelectorAll('.skills__progress-item-subtext'),
-    lines = document.querySelectorAll('.skills__progress-line span');
+const counters = document.querySelectorAll(".skills__progress-item-subtext"),
+  lines = document.querySelectorAll(".skills__progress-line span");
 
 counters.forEach((item, i) => {
-    lines[i].style.width = item.innerHTML;
+  lines[i].style.width = item.innerHTML;
+});
+
+document.addEventListener("scroll", function () {
+  const animatedBlock = document.getElementById("animatedBlock");
+  const blockPosition = animatedBlock.getBoundingClientRect().top;
+  const screenPosition = window.innerHeight / 1.5; // Позиция, при которой блок должен появиться
+
+  if (blockPosition < screenPosition) {
+    animatedBlock.classList.add("visible");
+  }
 });
